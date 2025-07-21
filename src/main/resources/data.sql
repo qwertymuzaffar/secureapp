bfe345a3a82ff95084a447a36dca0964e4127a34
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-21 15:40:58 -0400

    Schema Initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (title, author, price)
+VALUES ('Spring Boot Essentials', 'Jane Doe', 29.99);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books
+(
+    id     INT AUTO_INCREMENT PRIMARY KEY,
+    title  VARCHAR(255),
+    author VARCHAR(255),
+    price DOUBLE
+);
